1356b662f333c9824bcef2fc9055e57beb25fba9
Kleidson Alves <kleidsonalves15@gmail.com>
2022-12-27 15:38:08 -0300

    Ajuste da página de inserção de dados no modo retrato

--- Assets/Resources/AddInfoManager.cs
+++ Assets/Resources/AddInfoManager.cs
@@ -1,13 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AddInfoManager : MonoBehaviour
 {
     [SerializeField]
-    private string scene; 
+    private string scene, name;
+    private float weight;
+    
+    public Text age;
+   
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
 
+    public void Validate(string data)
+    {
+        try
+        {
+            Debug.Log(age.ToString());
+           // age = int.Parse(data);
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+
+    }
 
     public void Confirm()
     {
