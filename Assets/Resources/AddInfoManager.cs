98b1a23f59d61f28d8206083692c52eb4e109b16
Kleidson Alves <kleidsonalves15@gmail.com>
2022-12-27 16:49:35 -0300

    Adição de validação para input e transição de cenários

--- Assets/Resources/AddInfoManager.cs
+++ Assets/Resources/AddInfoManager.cs
@@ -4,37 +4,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class AddInfoManager : MonoBehaviour
 {
     [SerializeField]
-    private string scene, name;
+    private string scene;
+    private string name; 
+    private int age;
     private float weight;
+
+    public TMP_InputField  nameField, ageField, weightField;
     
-    public Text age;
-   
     void Start()
     {
         Screen.orientation = ScreenOrientation.Portrait;
+
+        age = PlayerPrefs.GetInt("age", 0);
+        weight = PlayerPrefs.GetFloat("weight", 0);
+        name = PlayerPrefs.GetString("name", "");
+
+        Debug.Log(name);
+
+        Next();
     }
 
-    public void Validate(string data)
+    private void SavePlayerData()
+    {
+        PlayerPrefs.SetInt("age", age);
+        PlayerPrefs.SetFloat("weight", weight);
+        PlayerPrefs.SetString("name", name);
+    }
+
+    public void Validate(string field)
     {
         try
         {
-            Debug.Log(age.ToString());
-           // age = int.Parse(data);
-
+           if(field == "name")
+           {
+                name = nameField.text; 
+           } 
+           else if (field == "age") 
+           {
+                age = int.Parse(ageField.text);
+           }
+           else
+           {
+                weight = float.Parse(weightField.text);
+           }
+             
         }
         catch (Exception e)
         {
+            if(field == "age")
+            {
+                age = 0;
+            }
+            else if(field == "weight")
+            {
+                weight = 0;
+            }
             Debug.LogException(e);
         }
 
     }
 
-    public void Confirm()
+    public void Next()
     {
-        SceneManager.LoadScene(scene);
+
+        if (name.Length != 0 && weight != 0 && age != 0)
+        {
+            SavePlayerData();
+            Debug.Log(age.ToString() + " " + weight.ToString() + " " + name);
+
+            SceneManager.LoadScene(scene);
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+        }
     }
 }
