3545d5fe2b6a80535ab967cf5b2c9a961d0505ca
Enzo Saraiva <eggqsaraiva@gmail.com>
2023-01-19 00:16:27 -0300

    Cria botão selecionar circuito

--- Assets/_Scripts/Camera/InteractiveObject.cs
+++ Assets/_Scripts/Camera/InteractiveObject.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class InteractiveObject : MonoBehaviour
 {
+    [SerializeField] private Color inactiveColor;
+    [SerializeField] private Color gazedAtColor;
+
+    public bool isGazed = false;
+    
+    private MeshRenderer myRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRenderer = GetComponent<MeshRenderer>();
+        myRenderer.material.color = inactiveColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnPointerEnter()
+    {
+        GazeAt(true);
+        isGazed = true;
+    }
+
+    public void OnPointerExit()
+    {
+        GazeAt(false);
+        isGazed = true;
+    }
+
+    public void GazeAt(bool gazing)
+    {
+        if(gazing)
+        {   
+            StartCoroutine(RemoveAfterSeconds(2));
+        } else
+        {
+            myRenderer.material.color = inactiveColor;
+        }
+    }
+
+    IEnumerator RemoveAfterSeconds (int seconds){
+        yield return new WaitForSeconds(seconds);
+
+        if(isGazed){
+            myRenderer.material.color = gazedAtColor;
+            gameObject.SetActive(false);
+        }
     }
 }
