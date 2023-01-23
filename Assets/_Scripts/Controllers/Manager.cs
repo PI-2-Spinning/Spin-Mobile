06bbccdd8d8f9d33d41ad21385e3583268006883
Lucas Rodrigues <lucasro229@gmail.com>
2023-01-23 20:20:52 -0300

    Recebimento de dados por bluetooth

--- Assets/_Scripts/Controllers/Manager.cs
+++ Assets/_Scripts/Controllers/Manager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Google.XR.Cardboard;
@@ -5,12 +6,17 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    string deviceName = "ESP32";
+    bool isConnected = false;
+
     // Start is called before the first frame update
     void Start()
     {  
         XRController.initialSetup();
         GeneralController context = GeneralController.getGeneralControllerInstance();
         Debug.Log("s");
+
+        BluetoothService.CreateBluetoothObject();
     }
 
     // Update is called once per frame
@@ -32,6 +38,26 @@ public class Manager : MonoBehaviour
                 XRController.EnterVR();
                 Debug.Log("Entered");
             }*/
+        }
+
+        if (isConnected)
+        {
+            try
+            {
+                string dataIn = BluetoothService.ReadFromBluetooth();
+                if (dataIn.Length > 0)
+                {
+                    Debug.Log(dataIn);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+        }
+        else
+        {
+            isConnected = BluetoothService.StartBluetoothConnection(deviceName);
         }
     }
 }
